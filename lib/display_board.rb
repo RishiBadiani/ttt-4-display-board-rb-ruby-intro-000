73699ff5b59e609985e7ed5bef432323b4449f55
Rishi Badiani <rishib9000@hotmail.com>
2019-01-06 22:42:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,45 +2,45 @@
 # out the current state.
 
 def display_board(board)
-  puts " #{{board[0]}}  | #{{board[1]}}   | #{{board[2]}}  "
+  puts "   |   |   "
   puts "-----------"
-  puts " #{{board[3]}}  | #{{board[4]}}   | #{{board[5]}}  "
+  puts "   |   |   "
   puts "-----------"
-  puts " #{{board[6]}}  | #{{board[7]}}  | #{{board[8]}}  "
+  puts "   |   |   "
+
+ puts "Turn 1"
+ board= [" "," "," "," "," "," "," "," "," "]
+ display_board(board)
+
+ puts "Turn 2"
+ board= [" "," "," "," ","X"," "," "," "," "]
+ display_board(board)
+
+ puts "Turn 3"
+ board= ["O"," "," "," "," "," "," "," "," "]
+ display_board(board)
+
+ puts "Turn 4"
+ board= ["O"," "," "," ","X"," "," "," "," "]
+ display_board(board)
+
+ puts "Turn 5"
+ board= ["X","X","X"," "," "," "," "," "," "]
+ display_board(board)
+
+ puts "Turn 6"
+ board= [" "," "," "," "," "," ","O","O","O"]
+ display_board(board)
+
+ puts "Turn 6"
+ board= ["X"," "," "," ","X"," "," "," ","X"]
+ display_board(board)
+
+ puts "Turn 7"
+ board= [" "," ","O"," ","O"," ","O"," "," "]
+ display_board(board)
+
+ puts "Turn 8"
+ board= ["X","O","X","X","O","O"," "," "," "]
+ display_board(board)
 end
-
-puts "Turn 1"
-board= [" "," "," "," "," "," "," "," "," "]
-display_board(board)
-
-puts "Turn 2"
-board= [" "," "," "," ","X"," "," "," "," "]
-display_board(board)
-
-puts "Turn 3"
-board= ["O"," "," "," "," "," "," "," "," "]
-display_board(board)
-
-puts "Turn 4"
-board= ["O"," "," "," ","X"," "," "," "," "]
-display_board(board)
-
-puts "Turn 5"
-board= ["X","X","X"," "," "," "," "," "," "]
-display_board(board)
-
-puts "Turn 6"
-board= [" "," "," "," "," "," ","O","O","O"]
-display_board(board)
-
-puts "Turn 6"
-board= ["X"," "," "," ","X"," "," "," ","X"]
-display_board(board)
-
-puts "Turn 7"
-board= [" "," ","O"," ","O"," ","O"," "," "]
-display_board(board)
-
-puts "Turn 7"
-board= ["X","O","X","X","O","O"," "," "," "]
-display_board(board)
