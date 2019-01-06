16eff4fe47a53b4d62349de672dc06fc124cc641
Rishi Badiani <rishib9000@hotmail.com>
2019-01-06 21:56:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,24 +7,14 @@ def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-
-  board[0] = [" "," "," "," "," "," "," "," "," "]
-  board[1] = [" "," "," "," ","X"," "," "," "," "]
-  board[2] = ["O"," "," "," "," "," "," "," "," "]
-  board[3] = ["O"," "," "," ","X"," "," "," "," "]
-  board[4] = ["X","X","X"," "," "," "," "," "," "]
-  board[5] = [" "," "," "," "," "," ","O","O","O"]
-  board[6] = ["X"," "," "," ","X"," "," "," ","X"]
-  board[7] = [" "," ","O"," ","O"," ","O"," "," "]
-  board[8] = ["X","O","X","X","O","O"," "," "," "]
 end
 
-display_board(board[0])
-display_board(board[1])
-display_board(board[2])
-display_board(board[3])
-display_board(board[4])
-display_board(board[5])
-display_board(board[6])
-display_board(board[7])
-display_board(board[8])
+display_board(board= [" "," "," "," "," "," "," "," "," "])
+display_board(board= [" "," "," "," ","X"," "," "," "," "])
+display_board(board= ["O"," "," "," "," "," "," "," "," "])
+display_board(board= ["O"," "," "," ","X"," "," "," "," "])
+display_board(board= ["X","X","X"," "," "," "," "," "," "])
+display_board(board= [" "," "," "," "," "," ","O","O","O"])
+display_board(board= ["X"," "," "," ","X"," "," "," ","X"])
+display_board(board= [" "," ","O"," ","O"," ","O"," "," "])
+display_board(board= ["X","O","X","X","O","O"," "," "," "])
