39fe01f3cfb36fc66c99bd138bae14d9e2241bee
Rishi Badiani <rishib9000@hotmail.com>
2019-01-06 18:30:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = puts "   |   |   "
-        puts "-----------"
-        puts "   |   |   "
-        puts "-----------"
-        puts "   |   |   "
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
 
 def display_board(board)
-  puts board
+  puts
 end
